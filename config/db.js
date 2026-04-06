e54418322ed3eb04b04bd744edd6e2f5b062291d
SINDHAV DINESH <sindhavdinesh93@gmail.com>
2026-04-06 16:05:44 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,21 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(
-  'mongodb+srv://ds3568065_db_user:Dinesh123@cluster0.ev8bqv9.mongodb.net/bookstore',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
-
+mongoose.connect('mongodb://ds3568065_db_user:Dinesh123@ac-deyrdkz-shard-00-00.ev8bqv9.mongodb.net:27017,ac-deyrdkz-shard-00-01.ev8bqv9.mongodb.net:27017,ac-deyrdkz-shard-00-02.ev8bqv9.mongodb.net:27017/bookstore?ssl=true&replicaSet=atlas-xxg05p-shard-0&authSource=admin&retryWrites=true&w=majority')
 const db = mongoose.connection;
+db.once('open', (err) => {
+    if (err) {
+        console.log(err);
+        return;
 
-db.on('error', (err) => {
-  console.log(err);
-});
-
-db.once('open', () => {
-  console.log('DB is connected successfully');
-});
-
+    }
+    console.log('DB is connected successfully');
+})
 module.exports = db;
